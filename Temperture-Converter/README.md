2e144fc6eaee382717fab122acee18c75311b5f9
123Devanshi <devanshirawat846@gmail.com>
2023-07-30 11:32:26 +0530

    Initial commit

---
+++ Temperture-Converter/README.md
@@ -0,0 +1,3 @@
+# Temperture-Converter
+This is a program that converts temperature from centigrade to fahrenheit and vice versa.
+HTML, CSS, Javascript are used.
---
+++ Temperture-Converter/script.js
@@ -0,0 +1,25 @@
+let cel = document.getElementById("cel");
+let far = document.getElementById("far");
+
+
+
+cel.addEventListener("input", function () {
+  let c = this.value;
+  let f = (c * 9) / 5 + 32;
+  if (!Number.isInteger(f)) {
+    f = f.toFixed(3);
+  }
+  far.value = f;
+});
+
+far.addEventListener("input", function () {
+  let f = this.value;
+  let c = (f * 9) / 5 + 32;
+  if (!Number.isInteger(c)) {
+    c = c.toFixed(3);
+  }
+  cel.value = c;
+});
+
+// Number.isInteger(f) : to check if a number is an integer
+// f.toFixed(4) : to limit the number to four decimal places
